668308c23c3e2334b7ee07062228183f80938161
chemiX <iam@chemix.cz>
2014-06-12 14:05:38 +0200

    added blank Import Presenter

---
+++ app/presenters/ImportPresenter.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Honza Cerny <hello@honzacerny.com>
+ */
+
+namespace App\Presenters;
+
+use Nette,
+	App\Model;
+
+
+/**
+ * Import presenter.
+ */
+class ImportPresenter extends BasePresenter
+{
+
+	public function renderDefault()
+	{
+
+	}
+
+}
