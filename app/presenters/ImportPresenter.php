e471528d4265d6ee9487a0fbb0401a59a04a3d4a
chemiX <iam@chemix.cz>
2014-06-12 17:55:56 +0200

    added cache for request

--- app/presenters/ImportPresenter.php
+++ app/presenters/ImportPresenter.php
@@ -12,6 +12,7 @@ use Facebook\FacebookRequest;
 use Facebook\FacebookRequestException;
 use Facebook\FacebookSession;
 use Tracy\Dumper;
+use Nette\Caching\Cache;
 
 
 /**
@@ -19,23 +20,35 @@ use Tracy\Dumper;
  */
 class ImportPresenter extends BasePresenter
 {
+	/**
+	 * @var \Nette\Caching\IStorage @inject
+	 */
+	public $cacheStorage;
 
 	public function renderDefault()
 	{
 		FacebookSession::setDefaultApplication('YOUR_APP_ID', 'YOUR_APP_SECRET');
 
 		$session = FacebookSession::newAppSession();
-		$data = array();
+		$cache = new Cache($this->cacheStorage, 'facebookWall');
+		$data = $cache->load("stories");
 
-		try {
-			$request = new FacebookRequest($session, 'GET', '/nettefw/feed');
-			$response = $request->execute();
-			$posts = $response->getGraphObject()->asArray();
+		if (empty($data)) {
+			try {
+				$request = new FacebookRequest($session, 'GET', '/nettefw/feed');
+				$response = $request->execute();
+				$posts = $response->getGraphObject()->asArray();
 
-			$data = $posts['data'];
-		} catch (\Exception $ex) {
-			throw $ex;
-			$this->terminate();
+				$data = $posts['data'];
+				$cache->save("stories", $data, array(
+					Cache::EXPIRATION => '+30 minutes',
+					Cache::SLIDING => TRUE
+				));
+
+			} catch (\Exception $ex) {
+				throw $ex;
+				$this->terminate();
+			}
 		}
 
 		Dumper::dump($data);
