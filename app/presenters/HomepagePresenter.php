7fed0d0a70ac7472a1b70d696f18fae4e2993e52
chemiX <iam@chemix.cz>
2014-06-13 19:48:29 +0200

    show posts on homepage

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -12,8 +12,15 @@ use Nette,
 class HomepagePresenter extends BasePresenter
 {
 
+	/**
+	 * @var \Nette\Database\Context @inject
+	 */
+	public $database;
+
 	public function renderDefault()
 	{
+		$facebookWallPosts = $this->database->table('facebook_wallposts')->where('status','1')->limit(5)->fetchAll();
+		$this->template->wallPosts =  $facebookWallPosts;
 	}
 
 }
